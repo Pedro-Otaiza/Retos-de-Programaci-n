ca73f8cbfe16cfb04da88b798e7ab5c06bbeb88c
Pedro Reyes Otaiza <pedro@inge-sec.com>
2024-04-01 18:55:58 -0300

    subiendo el primer reto, aun no esta terminado

---
+++ sintaxis.js
@@ -0,0 +1,24 @@
+/*  ------------------------------ EJERCICIO -----------------------------------------------------------------------------------
+1- Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+2- Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje(en una línea, varias...).
+3- Crea una variable(y una constante si el lenguaje lo soporta).
+4- Crea variables representando todos los tipos de datos primitivos del lenguaje(cadenas de texto, enteros, booleanos...).
+5- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ --------------------------------------------------------------------------------------------------------------------------------
+*/
+
+
+/* No es el sitio oficial pero es un gran sitio para obtener la documentación de javascript https://developer.mozilla.org/es/docs/Web/JavaScript */
+
+// Este es un comentario de una linea.
+
+/*
+Este es un comentario de varias lineas
+*/
+
+// ------- VARIABLES --------------------
+let miVariable = "JavaScript"; //Variable let para asignar valor a la variable.
+const miConstante = "Hello "; //Constante con el valor de Pi en JavaScript.
+
+// ------- DATOS PRIMITIVOS -------------
+console.log(miConstante + miVariable);
